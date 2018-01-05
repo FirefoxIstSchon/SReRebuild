c8825bb2b0227a7fcc9793f6d229aad9829c6437
K <31030673+FirefoxIstSchon@users.noreply.github.com>
2018-01-05 23:36:01 +0300

    scales up and running

--- app/src/main/java/designersfox/k/s_rerebuild/factory/ScaleFactory.java
+++ app/src/main/java/designersfox/k/s_rerebuild/factory/ScaleFactory.java
@@ -4,6 +4,8 @@ import designersfox.k.s_rerebuild.model.Scale;
 import designersfox.k.s_rerebuild.technical.ScaleBuilder;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Environment;
 import java.util.ArrayList;
 
 public class ScaleFactory {
@@ -11,6 +13,9 @@ public class ScaleFactory {
     static volatile ScaleFactory scaleFactory;
 
     static ScaleBuilder scaleBuilder;
+
+    MediaPlayer currentSound;
+    String sdPath;
 
     private ScaleFactory(){
         scaleBuilder = ScaleBuilder.getInstance();
@@ -28,14 +33,35 @@ public class ScaleFactory {
     public Scale getScale(int key, int type, int octaves, Activity activity){
         Scale returnScale = new Scale(key, type, octaves);
         ArrayList<Integer> soundBuild = scaleBuilder.getSoundBuild(key, type, octaves);
+        currentSound = new MediaPlayer();
         for(Integer sound : soundBuild){
-            //todo: returnScale.sounds.add( system.path + String.valueOf(sound) );
+            sdPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+            String currentSoundPath = sdPath + "/msounds/p" + sound + ".mp3";
+            try {
+                currentSound.setDataSource(currentSoundPath); //todo: error verirse burada MediaPlayer.create gerekebilir
+            } catch (Throwable t) {t.printStackTrace();}
+            returnScale.sounds.add(currentSound); //todo: her yerde ayni ses cikarsa bu obj pass'inden
         }
         returnScale.notesAsText = scaleBuilder.getNotesFromSoundBuild(soundBuild);
+        currentSound.release(); //todo: error veriyor mu?
         return returnScale;
     }
 
+    /*mp.prepare();
+                mp.start();*/
 
+    //avoid exceptions
+     /* try {
+        mp.reset();
+        mp.prepare();
+        mp.stop();
+        mp.release();
+        mp=null;
+    }
+  catch (Exception e)
+    {
+        e.printStackTrace();
+    }*/
 
 
 }
